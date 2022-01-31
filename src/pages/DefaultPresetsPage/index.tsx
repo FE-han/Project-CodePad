3cbab24222319fe0a474c1ee280c4d516a83cea3
정경훈 <ruderud0055@gmail.com>
2022-01-31 11:49:35 +0900

    feat : 디폴트 프리셋 페이지 생성 및 구역분리

--- src/pages/DefaultPresetsPage/index.tsx
+++ src/pages/DefaultPresetsPage/index.tsx
@@ -5,9 +5,31 @@ const DefaultPresetsPageStyles = makeStyles({
   root: {
     background: "orange",
 
-    margin: "0 auto 0 auto",
-    width: "880px",
-    height: "100%",
+    padding: "35px 60px 35px 60px",
+
+    display: "grid",
+    gridTemplateColumns: "1fr 1fr",
+    gridTemplateRows: "150px 350px 200px",
+    gridColumnGap: "100px",
+    gridRowGap: "20px",
+    gridTemplateAreas: `
+    "launchPad togglePresetBtn"
+    "launchPad presetList"
+    "none presetList"`,
+
+    "& > *": {
+      border: "1px solid gray",
+      minWidth: "500px",
+    },
+  },
+  launchPad: {
+    gridArea: "launchPad",
+  },
+  togglePresetBtn: {
+    gridArea: "togglePresetBtn",
+  },
+  presetList: {
+    gridArea: "presetList",
   },
 });
 
@@ -15,8 +37,19 @@ export function DefaultPresetsPage() {
   const classes = DefaultPresetsPageStyles();
   return (
     <div className={classes.root}>
-      대충 디폴트 프리셋 컴포넌트들 올 곳
-      <Link to={"/intro"}>인트로 페이지 이동버튼</Link>
+      <div className={classes.launchPad}>
+        <Link to={"/intro"}>인트로 페이지 이동버튼</Link>
+        런치패드 올곳
+        {/* <LaunchPad /> */}
+      </div>
+      <div className={classes.togglePresetBtn}>
+        디폴트 프리셋 {"<->"} 마이프리셋 토글 버튼 올곳
+        {/* <PresetToggleBtn /> */}
+      </div>
+      <div className={classes.presetList}>
+        프리셋 리스트 올곳
+        {/* <PresetList /> */}
+      </div>
     </div>
   );
 }
