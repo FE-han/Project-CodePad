b615675323daa233b36ce87314819466e2c363ee
sunky97 <hsk11261612@gmail.com>
2022-02-10 19:50:12 +0900

    feat: json-server, mock data 작성

--- src/api/getUserInfo.ts
+++ src/api/getUserInfo.ts
@@ -9,6 +9,8 @@ export async function getUserInfo(params: UserIdParams) {
   const config: AxiosRequestConfig = {
     //token input
   };
-  const response = await axiosInstance(config).get(`/presets/userInfo/${params.userId}`);
+  // const response = await axiosInstance(config).get(`/presets/userInfo/${params.userId}`);
+  //json-server 용
+  const response = await axiosInstance(config).get(`/userInfo/${params.userId}`);
   return response.data;
 }
