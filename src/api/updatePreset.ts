c14d7d5187894f21052a3374d38a53d4736cb31e
sunky97 <hsk11261612@gmail.com>
2022-02-19 12:19:06 +0900

    fix: updatepreset api 에러 수정

--- src/api/updatePreset.ts
+++ src/api/updatePreset.ts
@@ -1,11 +1,9 @@
 import { AxiosRequestConfig } from "axios";
-import { formDataTypes } from "../pages/HandleMyPresetPage";
 import { axiosInstance } from "./axiosInstance";
 
 export async function updatePreset(formData: any, presetId: string) {
   
   const config: AxiosRequestConfig = {
-    baseURL: "http://localhost:4200",
     //token input
     headers: {
       "Content-type": "multipart/form-data",
